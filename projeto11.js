67d403e0c6d0f721020685fdf73594561005cca3
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-09-17 10:05:20 -0300

    atualização

--- projeto11.js
+++ projeto11.js
@@ -1,9 +1,22 @@
-// 1) criar uma função somar que retorna a soma de todos os múltiplos de 3 e 5.
+// 1) criar uma função somar que retorna a soma de todos os múltiplos de 3 e 5. só soma os múltiplos de 3 e 5
 
+// primeiramente temos que saber quais são os multiplos de cinco e tres e trabalharemos dentro de um limite
+// armazenar os múltiplos de 3
+// armazenar os múltiplos de 5
+//somar os resultados
+somar = (10);
 
-somar = 10;
 
 function somar (limite) {
+let multiploDeTres = 0;          // vamos declarar essas variáveis e atribuir um valor zero para as duas
+let multiploDeCinco = 0;
+for (i = 0; i<= limite; i++) {
+    if (i % 3 === 0)
+    multiploDeTres += i;
+    if (i % 5 === 0)
+    multiploDeCinco += i;
+}
+console.log(multiploDeTres + multiploDeCinco);
 
 }
 
