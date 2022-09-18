7a7496d8b96b786ee62f98ec680f70b74091ea12
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-09-17 21:19:14 -0300

    modificações

--- projeto15.js
+++ projeto15.js
@@ -1,4 +1,4 @@
-let valor =Math.random()
+/*let valor =Math.random()
 console.log (valor)
 
 
@@ -9,4 +9,31 @@ const array = [1, 3, 2];
 
 console.log(Math.max(...array));
 
-// parei em 3:10:08
+// parei em 3:10:08 */
+
+// 1) criar uma função que some 2 números
+
+soma(5,3);
+function soma (numero1,numero2) {
+    const resultado = numero1 + numero2
+   
+    console.log('a soma é',resultado)
+}
+
+// 2) função que exibe e soma valores de 1 a 10
+// fiz duas coisas: exibi os valores depois somei
+exibeSoma(10)
+
+function exibeSoma (limite) {
+  
+    for (let i = 1; i<= limite; i++) {
+              console.log (i)
+    }
+  
+}
+function acumula(n) {
+    var total = 0;
+    for (var i = 1; i <= n; i++) total += i;
+    return total;
+}
+console.log(acumula(10));
